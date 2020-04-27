f0720506738224b65ec5c0cdee1cd374b4b11c2a
Shawn Beaton <beat0154@algonquinlive.com>
2020-04-27 15:00:01 -0400

    V1.0.3

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function newView(file,value){
 		document.getElementById("main").innerHTML = data;
 	});
 }
+function resize(){
+    var main = document.getElementById('main');
+  if(window.innerWidth > 800){
+    var myHeight = window.innerHeight;
+    var newHeight = myHeight - 129;
+    var newHeight = newHeight.toString().concat("px");
+    main.style.height = newHeight;
+  }else{
+    main.style.height = "auto";
+  }
+}
+function copy(that){
+    var inp =document.createElement('input');
+    document.body.appendChild(inp)
+    inp.value =that.textContent
+    inp.select();
+    document.execCommand('copy',false);
+    inp.remove();
+}
